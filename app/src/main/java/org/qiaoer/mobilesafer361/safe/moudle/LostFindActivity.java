f98c7c04dba73107ef2f37b152722f77999b77ee
qiaoer <qiaoer2017@163.com>
2016-05-15 22:12:52 +0800

    SetUp4Activity完成

---
+++ app/src/main/java/org/qiaoer/mobilesafer361/safe/moudle/LostFindActivity.java
@@ -0,0 +1,15 @@
+package org.qiaoer.mobilesafer361.safe.moudle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.qiaoer.mobilesafer361.R;
+
+public class LostFindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lostfind);
+    }
+}
--- app/src/main/java/org/qiaoer/mobilesafer361/safe/moudle/SetUp4Activity.java
+++ app/src/main/java/org/qiaoer/mobilesafer361/safe/moudle/SetUp4Activity.java
@@ -1,12 +1,65 @@
 package org.qiaoer.mobilesafer361.safe.moudle;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class SetUp4Activity extends AppCompatActivity {
+import org.qiaoer.mobilesafer361.R;
+
+public class SetUp4Activity extends BaseSetUpActivity {
+    private TextView mStatusTextView;
+    private ToggleButton mToggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup4);
+        initView();
+    }
+
+    private void initView() {
+        ((RadioButton) findViewById(R.id.fourthRadioButton)).setChecked(true);
+        mStatusTextView = (TextView) findViewById(R.id.setup4StatusTextView);
+        mToggleButton = (ToggleButton) findViewById(R.id.securityFunctionToggleButton);
+        boolean protecting = sp.getBoolean("protecting", true);
+        if (protecting) {
+            mStatusTextView.setText("防盗保护已经开启");
+            mToggleButton.setChecked(true);
+        } else {
+            mStatusTextView.setText("防盗保护还未开启");
+            mToggleButton.setChecked(false);
+        }
+
+        mToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    mStatusTextView.setText("防盗保护已经开启");
+                } else {
+                    mStatusTextView.setText("防盗保护还未开启");
+                }
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putBoolean("protecting", isChecked);
+                editor.commit();
+            }
+        });
+
+    }
+
+    @Override
+    public void showPre() {
+        startActivityAndFinishSelf(SetUp3Activity.class);
+    }
+
+    @Override
+    public void showNext() {
+        //跳转至防盗保护页面
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("isSetUp", true);
+        editor.commit();
+        startActivityAndFinishSelf(LostFindActivity.class);
     }
 }
